d002709ae03c5c2ebd27e36e027377456cee7236
mrduckydoo <moop_doopy@protonmail.com>
2023-01-25 08:55:47 +0100

    format stuff

--- src/entry.c
+++ src/entry.c
@@ -1,11 +1,12 @@
 #include "common/ultra_protocol.h"
 #include "sys/x86_64/utils.h"
 
-void main(struct ultra_boot_context *boot_ctx, uint32_t magic)
+void
+main(struct ultra_boot_context* boot_ctx, uint32_t magic)
 {
-	while(magic != ULTRA_MAGIC)
-		hang();
+  while (magic != ULTRA_MAGIC)
+    hang();
 
-    while(1)
-		asm volatile("hlt");
+  while (1)
+    asm volatile("hlt");
 }
--- src/sys/x86_64/utils.h
+++ src/sys/x86_64/utils.h
@@ -1,3 +1,8 @@
-#define hang() do { asm volatile("cli"); while(1) asm volatile("hlt"); } while(0)
+#define hang()                                                                 \
+  do {                                                                         \
+    asm volatile("cli");                                                       \
+    while (1)                                                                  \
+      asm volatile("hlt");                                                     \
+  } while (0)
 
 #define _PACKED_ __attribute__((packed))
